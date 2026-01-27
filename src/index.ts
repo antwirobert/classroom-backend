d82d77ea2193553fba7e61a6d6169d0376a6fef7
Robret Antwi <robertantwi84@gmail.com>
2026-01-27 19:31:52 +0000

    implement security middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import subjectsRouter from "./routes/subjects";
+import securityMiddleware from "./middleware/security";
 
 const app = express();
 const PORT = 3000;
@@ -14,6 +15,8 @@ app.use(
 );
 
 app.use(express.json());
+
+app.use(securityMiddleware);
 
 app.use("/api/subjects", subjectsRouter);
 
